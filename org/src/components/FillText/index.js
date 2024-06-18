672909e6c6d38e641a484005e54eb551578b3f84
Sebastian Castro <scastro@qvision.com.co>
2024-06-17 19:36:21 -0500

    [sc] create components Form, FillText

---
+++ org/src/components/FillText/index.js
@@ -0,0 +1,14 @@
+import './FillText.css'
+
+const FillText = () =>{
+    return <div className =" fill-text">
+        <label htmlFor="">Nombre</label>
+        <input 
+        type="text"
+        placeholder="Ingresar Nombre" 
+        
+        />
+    </div>
+}
+
+export default FillText;
---
+++ org/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import FillText from "../FillText";
+import "./Form.css"
+
+const Form = () =>{
+    return<section className = "form">
+        <form action="">
+            <h2>Rellene el formulario para crear el colaborador</h2>
+            <FillText />
+        </form>
+    </section>
+}
+
+export default Form;
